692d37dbc7cfce52f44122e8e336cd36b28cb342
Mark Heath <mark.heath@gmail.com>
2012-02-28 10:47:13 +0000

    first version with a hexagon and a rectangle with rounded sides

---
+++ WpfShapes/Hexagon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace WpfShapes
+{
+    public class Hexagon : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get { return GetGeometry(); }
+        }
+
+        private Geometry GetGeometry()
+        {
+            double sideLength = 100;
+            double x = Math.Sqrt(sideLength * sideLength / 2);
+            return Geometry.Parse(String.Format("M {0},0 h {1} l {0},{0} l -{0},{0} h -{1} l -{0},-{0} Z", x, sideLength));
+        }
+    }
+}
---
+++ WpfShapes/RoundedSidesRectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace WpfShapes
+{
+    public class RoundedSidesRectangle : Shape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get { return GetGeometry(); }
+        }
+
+        private Geometry GetGeometry()
+        {
+            return Geometry.Parse("M 20,10 h 100 a 100,100,45,0,1,0,100 h -100 a 100,100,-45,0,1,0,-100 Z");
+        }
+    }
+}
