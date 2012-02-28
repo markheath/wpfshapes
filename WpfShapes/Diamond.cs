df8aa6a7993d521aeafdbe6a3fa52cb23dd0b6bd
Mark Heath <147668+markheath@users.noreply.github.com>
2012-02-28 16:59:43 +0000

    added a diamond shape

---
+++ WpfShapes/Diamond.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace WpfShapes
+{
+    public class Diamond : Shape
+    {
+        public Diamond()
+        {
+            this.Stretch = Stretch.Fill;
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get { return GetGeometry(); }
+        }
+
+        private Geometry GetGeometry()
+        {
+            return Geometry.Parse("M 100, 0 l 100, 100 l -100, 100 l -100, -100 Z");
+        }
+    }
+}
